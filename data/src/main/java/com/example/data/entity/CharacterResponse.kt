6f2ef902097e5756ef7892054e09bbcc75f8655d
Andrey Nekrasov <boss.nekrass@mail.ru>
2022-10-05 14:51:44 +0300

    q121

--- data/src/main/java/com/example/data/entity/CharacterResponse.kt
+++ data/src/main/java/com/example/data/entity/CharacterResponse.kt
@@ -26,7 +26,7 @@ data class CharacterResponse(
             species = species,
             gender = gender,
             imageUrl = image,
-            location = location.toDomainObject()//1
+            location = location.toDomainObject()//123
 
 
         )
